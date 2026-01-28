9afded049c5cbe2b256acdc72e635a794c31ef6a
Vindz1 <vindz1@gmail.com>
2026-01-28 06:33:49 +0100

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         await fetch(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
           method: 'POST',
           headers: { 'Authorization': `Bearer ${accessToken}`, 'Content-Type': 'application/json' },
-          body: JSON.stringify({ uris: uris.slice(0, 100) })
+          body: JSON.stringify({ uris: uris.slice(0, 50) })
         });
       }
       return res.status(200).json({ url: playlist.external_urls.spotify });
