5b13f00005b04d4821f6aa106ec881ee1460279b
Vindz1 <vindz1@gmail.com>
2026-01-28 18:56:02 +0100

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,8 +1,12 @@
 export default async function handler(req, res) {
   const { action, username, setlistId } = req.query;
   const apiKey = process.env.SETLIST_FM_API_KEY;
-  const headers = { 'x-api-key': apiKey, 'Accept': 'application/json', 'User-Agent': 'Festival-Rewind' };
-
+  const headers = { 
+    'x-api-key': apiKey, 
+    'Accept': 'application/json', 
+    'User-Agent': 'Festival-Rewind' 
+  };
+  
   try {
     if (action === 'user') {
       const response = await fetch(`https://api.setlist.fm/rest/1.0/user/${username}/attended?p=1`, { headers });
@@ -10,7 +14,7 @@ export default async function handler(req, res) {
       const data = await response.json();
       return res.status(200).json({ results: data.setlist || [] });
     }
-
+    
     if (action === 'songs') {
       const response = await fetch(`https://api.setlist.fm/rest/1.0/setlist/${setlistId}`, { headers });
       const s = await response.json();
