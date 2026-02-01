c68c7e9af167607e2394afe65433e001b50369b9
Vindz1 <vindz1@gmail.com>
2026-02-01 19:13:30 +0100

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -6,7 +6,7 @@ import { supabase } from '@/integrations/supabase/client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { toast } from 'sonner';
-import { useAuth } from "../AuthContext";.
+import { useAuth } from "../AuthContext";
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true);
