f03158e6a6b7cd5fb8ee1a9753cd57a45e10832b
Vindz1 <vindz1@gmail.com>
2026-02-08 19:10:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Success from './pages/Success';
 
 // IMPORT CRITIQUE : Utilise le chemin relatif direct
 import { AuthProvider } from "./AuthContext";
@@ -46,6 +47,7 @@ const App = () => (
             <Route path="/merch" element={<Shop />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/subscription" element={<Subscription />} />
+            <Route path="/success" element={<Success />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
