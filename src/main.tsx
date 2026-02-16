9e3bc3cad3e32e9742e629ff21ba36602eb4c8de
Vindz1 <vindz1@gmail.com>
2026-02-16 13:36:04 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { preventMusicTranslation } from './utils/preventTranslation';
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// Active la protection
+preventMusicTranslation();
